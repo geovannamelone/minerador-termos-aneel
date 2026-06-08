7b66bafb1adf597abc93e1340a6cd24e92cf2840
ninstyle <ninstyl.studies@gmail.com>
2026-06-08 00:01:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -528,7 +528,7 @@ if documentos:
                     data=buffer.getvalue(),
                     file_name="resultado_glossario.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                    key="download_excel_resultados"
+                    key=f"download_excel_resultados_{hash(buffer.getvalue())}"
                 )
     else:
                 st.warning(
